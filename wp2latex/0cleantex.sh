452cc972cc525bfb07f7183263e19c77f71101f3
John Baker <bakerjd99@gmail.com>
2022-02-22 11:26:44 -0700

    adjust 0cleantex.sh

--- wp2latex/0cleantex.sh
+++ wp2latex/0cleantex.sh
@@ -1,4 +1,4 @@
-# run in latex directory with $bash 0cleantex.sh
+# run in latex directory with $zsh 0cleantex.sh
 echo "removing latex temp files"
 rm *.aux
 rm *.bbl
@@ -15,4 +15,6 @@ rm *.ind
 rm *.ilg
 rm *.blg
 rm *.gz
+rm *.fls
+rm *.fdb_latexmk
 #rm *.gz(busy)
